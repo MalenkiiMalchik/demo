6254ef0bffc64ecca4790dd34d716bc623100e4d
MalenkiiMalchik <ajr166@georgetown.edu>
2023-09-13 13:44:46 -0400

    First Commit

---
+++ Scripts/github_demo.R
@@ -0,0 +1,18 @@
+#### Title
+# PURPOSE: Demo for Github
+# AUTHOR: alerichardson | sch
+# LICENSE: MIT
+# DATE: 2023-09-13
+# NOTES: 
+
+#### LOCALS & SETUP ============================================================================
+
+# Libraries
+require(tidyverse)
+require(gagglr)
+require(here)
+require(googledrive)
+si_setup()
+
+#### LOAD DATA ============================================================================  
+
